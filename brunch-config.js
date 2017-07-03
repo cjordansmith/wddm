0d2ba7bb5eca2f24fa877783db3db9c3608c5f79
Jordan Smith <cjordansmith@gmail.com>
2017-07-02 22:51:30 -0500

    initial commint to new repo, copied from wddm-class

---
+++ brunch-config.js
@@ -0,0 +1,22 @@
+// See http://brunch.io for documentation.
+exports.config = {
+
+	files: {
+		javascripts: { joinTo: {
+	      'js/app.js': [
+					'app/initialize.js',
+					'app/app.js',
+					'app/tablescript.js'
+				]
+			}
+		},
+		stylesheets: { joinTo: 'css/app.css' }
+	},
+
+	plugins: {
+		imageoptimizer: {
+			smushit: false,
+			path: 'assets/img'
+		}
+	}
+};
